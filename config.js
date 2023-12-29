e76ad262cbf95df4945f72481dff9e42f931c085
AlmaziikDev <almazgamer228@gmail.com>
2023-12-29 13:07:35 +0300

    dell

--- config.js
+++
@@ -1,9 +0,0 @@
-{
-    "dbd": {
-        "port": 80,
-        "domain": "https://almaz.is-a.dev",
-        "redirectUri": "/discord/callback",
-        "license": "dbdLicense",
-        "ownerIDs": ["718458417232085104, 695642322695749663"]
-    }
-}
