f0ced88f7994c10c567c35ae58448ef90a5fc5d7
Almaz <almazgamer228@gmail.com>
2023-12-29 13:03:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
 {
-    "discord": {
-        "token": "NzcyNTIwNDY2MTY5NTkzODU3.GwZzeG.EdSEvhVd8lA9pxf6vVij1gHpUeF0I-xglq8KsU",
-        "client": {
-            "id": "772520466169593857",
-            "secret": "5iLXmkm3z2jWT_ch17j7f1B60-H0bC65"
-        }
-    },
     "dbd": {
         "port": 80,
         "domain": "https://almaz.is-a.dev",
