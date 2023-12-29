1a3fd8f2a3a6437d74b0a260a8ffb3e42531e87c
AlmaziikDev <almazgamer228@gmail.com>
2023-12-29 12:59:25 +0300

    Site

---
+++ config.js
@@ -0,0 +1,16 @@
+{
+    "discord": {
+        "token": "NzcyNTIwNDY2MTY5NTkzODU3.GwZzeG.EdSEvhVd8lA9pxf6vVij1gHpUeF0I-xglq8KsU",
+        "client": {
+            "id": "772520466169593857",
+            "secret": "5iLXmkm3z2jWT_ch17j7f1B60-H0bC65"
+        }
+    },
+    "dbd": {
+        "port": 80,
+        "domain": "https://almaz.is-a.dev",
+        "redirectUri": "/discord/callback",
+        "license": "dbdLicense",
+        "ownerIDs": ["718458417232085104, 695642322695749663"]
+    }
+}
